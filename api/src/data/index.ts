98048e985613f01eda3aae8ecb6a60f7229cbd08
baylesa-dev <adrien.bayles@epitech.eu>
2019-06-28 18:26:54 +0200

    Create investment schema

---
+++ api/src/data/index.ts
@@ -0,0 +1,14 @@
+import Investment, {
+    InvestmentModel,
+    InvestmentModelType,
+    InvestmentSchema,
+    InvestmentSchemaType
+} from './Investment'
+
+export {
+    Investment,
+    InvestmentModel,
+    InvestmentModelType,
+    InvestmentSchema,
+    InvestmentSchemaType
+}
