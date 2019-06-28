f66eb9c3b2c5c6c34aeaaea9c6b06ccbfab71d51
baylesa-dev <adrien.bayles@epitech.eu>
2019-06-28 17:01:47 +0200

    Instantiate server
    Create fastify server
    Register fastify plugins

---
+++ api/src/config.ts
@@ -0,0 +1,11 @@
+const config = {
+    server: {
+      port: 8080,
+      ip: 'localhost'
+    },
+    security: {
+      secret: '%|+Pi!k9@yR3kD2Db#+IdynT?sk&#+R@'
+    }
+}
+
+export default config
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,46 @@
-const main = async () => {
-    console.log('Hello word!')
+import fastify from 'fastify'
+// @ts-ignore
+import fastifyBlipp from 'fastify-blipp'
+import fastifyCookie from 'fastify-cookie'
+import fastifySession from 'fastify-session'
+
+import config from './config'
+
+
+const createServer = async() => {
+    console.info(`Creating HTTP server on 'http://${config.server.ip}:${config.server.port}'`)
+
+    const server = fastify({
+        logger: {
+            level: 'info'
+        }
+    })
+
+    // Plugins
+    await server.register(fastifyCookie)
+    await server.register(fastifySession, {
+        secret: config.security.secret,
+        cookie: {
+            secure: false
+        }
+    })
+    await server.register(fastifyBlipp)
+
+    // Listen
+    await server.listen(config.server.port, config.server.ip)
+
+    // Dump routes
+    // @ts-ignore
+    await server.blipp()
+}
+
+const main = async() => {
+    try {
+        await createServer()
+    } catch(e) {
+        console.error(e.message)
+        process.exit(1)
+    }
 }
 
 main()
